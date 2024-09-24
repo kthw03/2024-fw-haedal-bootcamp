8e907b802ca79750cbcbcbd03140923fd871abee
kthw03 <kthwan5132@naver.com>
2024-09-24 19:18:36 +0900

    4주차-10807

---
+++ hw4-10807.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int Num[N];
+    int v;
+    scanf("%d", &v);
+    int answer;
+    for (int i=0; i<N; i++) {
+        if (Num[i] == v)
+            answer += 1;
+    }
+    printf("%d", v);
+    return 0;
+}
