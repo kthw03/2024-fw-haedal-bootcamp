182d2c25fc690bf708b68ed2fc1870a2a022bb4d
kthw03 <kthwan5132@naver.com>
2024-10-12 18:12:50 +0900

    4주차-3952

--- hw4.3952.c
+++ hw4.3952.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 int main() {
-    int N[10];
+    int N;
     int num[10];
     int answer=0;
     for(int i=0;i<10;i++){
-        scanf("%d", &N[i]);
-        num[i] = N[i]%42;
+        scanf("%d", &N);
+        num[i] = N%42;
     }
     for(int i=0;i<10;i++){
+        int flag=0;
         for(int j=i+1;j<10;j++){
             if (num[i]==num[j])
-                answer+=1;
+                flag=1;
         }
+        if (flag==0)
+            answer += 1;
     }
-    printf("%d", 10-answer);
+    printf("%d", answer);
     return 0;
 }
