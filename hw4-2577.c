8e5b0c60ee4785bd9b850d8d89db26739e50b5a8
kthw03 <kthwan5132@naver.com>
2024-10-12 17:16:45 +0900

    4주차-2577

--- hw4-2577.c
+++ hw4-2577.c
@@ -6,18 +6,12 @@ int main() {
     scanf("%d", &C);
     int N = A * B * C;
     int answer[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int a = 1;
-    while (a*10<N) {
-        a *= 10;
+    
+    for(int i=N;i>0;i/=10){
+        answer[i%10]++;
     }
-    while (N>=10){
-        answer[N/a] += 1;
-        if (N%a == 0)
-            answer[0] += 1;
-        N = N%a;
-        a = a/10;
-    }
-    for (int i=0;i<9;i++){
+
+    for (int i=0;i<10;i++){
         printf("%d\n", answer[i]);
     }
     return 0;
