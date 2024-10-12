11ad788ff96b898582f4353e09d995863bcab158
kthw03 <kthwan5132@naver.com>
2024-10-12 17:58:23 +0900

    4주차-8958

---
+++ hw4-8958.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int n=0;
+    char s[80];
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+        int flag=0;
+        int answer=0;
+        scanf("%s", s);
+        for(int j=0;j<strlen(s);j++){
+            if (s[j]=='O'){
+                flag++;
+                answer += flag;
+            }
+            else
+                flag=0;
+        }
+        printf("%d", answer);
+    }
+    return 0;
+}
